53ad0e2a6c2f7139c85fab0351d87c518aa5dec7
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-06-13 15:47:18 +0200

    public OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import './App.css'
-import Profil from './Pages/Profil/Profil'
-import Public from './Pages/Public/Public'
-import RequireAuth from './auth/RequireAuth'
-import Layout from './components/Layout'
-import Nav from './components/Nav/Nav'
-import { Routes, Route } from 'react-router-dom'
-
+import "./App.css";
+import Profil from "./Pages/Profil/Profil";
+import Public from "./Pages/Public/Public";
+import RequireAuth from "./auth/RequireAuth";
+import Footer from "./components/Footer/Footer";
+import Layout from "./components/Layout";
+import Nav from "./components/Nav/Nav";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
- 
   return (
     <>
-  <Nav />
-  <Routes>
+      <Nav />
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />} />
           {/* <Route path="sign-in" element={<Login />} /> */}
           {/* <Route path="signup" element={<Signup />} /> */}
 
           <Route element={<RequireAuth />}>
-            <Route path='profil' element={<Profil />} />
+            <Route path="profil" element={<Profil />} />
           </Route>
         </Route>
       </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Public/Public.jsx
+++ src/Pages/Public/Public.jsx
@@ -1,7 +1,14 @@
+import Features from "../../components/Features/Features";
 import Hero from "../../components/Hero/Hero";
 
 function Public() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />;
+      <Features />;
+      
+    </>
+  );
 }
 
 export default Public;
