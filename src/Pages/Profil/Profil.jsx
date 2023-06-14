c7f6464cfefa2b2a2d4889853050644400e57b0d
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-06-14 19:16:42 +0200

    User page OK

--- src/Pages/Profil/Profil.jsx
+++ src/Pages/Profil/Profil.jsx
@@ -1,6 +1,6 @@
 import { useGetUserDataMutation } from "../../user/userApiSlice";
 import { setCredentials } from "../../user/userSlice";
-// import styles from "./Profil.module.css";
+import "./Profil.css";
 // import { getUserData } from "../../user/user";
 import {
   selectCurrentFirstname,
@@ -8,7 +8,7 @@ import {
 } from "../../user/userSlice";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./Profil.module.css";
+// import styles from "./Profil.module.css";
 import { useUpdateUserDataMutation } from "../../user/userApiSlice";
 
 function Profil() {
@@ -70,39 +70,80 @@ function Profil() {
   // console.log(firstName + " " + lastName);
 
   return (
-    <div className={styles.header}>
-      <h1>
-        Welcome back
-        <br />
-        {userFirstame} {userLastname}
-      </h1>
-      {editMode ? (
-        <div className={styles.editContainer}>
-          <input
-            className={styles.editInput}
-            type="text"
-            placeholder={userFirstame}
-            onChange={(e) => setFirstName(e.target.value)}
-            value={firstName}
-          />
-          <input
-            className={styles.editInput}
-            type="text"
-            placeholder={userLastname}
-            onChange={(e) => setLastName(e.target.value)}
-            value={lastName}
-          />
-          <button className={styles.editButton} onClick={updateProfile}>
-            Save
-          </button>
+    <>
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>
+            Welcome back
+            <br />
+            {userFirstame} {userLastname}
+          </h1>
+          {editMode ? (
+            <div className="editContainer">
+              <input
+                className="editInput"
+                type="text"
+                placeholder={userFirstame}
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+              />
+              <input
+                className="editInput"
+                type="text"
+                placeholder={userLastname}
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+              />
+              <div>
+                <button className="edit-button" onClick={updateProfile}>
+                  Save
+                </button>
+                <button className="edit-button" onClick={toggle}>
+                  Cancel
+                </button>
+              </div>
+            </div>
+          ) : null}
+          {!editMode ? (
+            <button className="edit-button" onClick={toggle}>
+              Edit Name
+            </button>
+          ) : null}
         </div>
-      ) : null}
-      {!editMode ? (
-        <button className={styles.editButton} onClick={toggle}>
-          Edit Name
-        </button>
-      ) : null}
-    </div>
+
+        <h2 className="sr-only">Accounts</h2>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
